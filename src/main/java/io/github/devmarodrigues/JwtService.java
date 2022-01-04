7c30080de737c62bb5d89cc7d16c03176c96d8f2
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2022-01-04 13:45:20 -0300

    Decodificando o token

--- src/main/java/io/github/devmarodrigues/JwtService.java
+++ src/main/java/io/github/devmarodrigues/JwtService.java
@@ -1,10 +1,11 @@
 package io.github.devmarodrigues;
 
 import io.github.devmarodrigues.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,7 @@ public class JwtService {
         LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expString);
         Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
         Date data = Date.from(instant);
+
         return Jwts
                 .builder()
                 .setSubject(usuario.getLogin())
@@ -35,11 +37,40 @@ public class JwtService {
                 .compact();
     }
 
+    private Claims obterClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido(String token) {
+        try {
+            Claims claims = obterClaims(token);
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime data = dataExpiracao.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return !LocalDateTime.now().isAfter(data);
+        }catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String obterUsuario(String token) throws ExpiredJwtException{
+        return (String) obterClaims(token).getSubject();
+    }
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(VendasApplication.class);
         JwtService jwtService = context.getBean(JwtService.class);
         Usuario usuario = Usuario.builder().login("Mardonio").build();
-        System.out.println(jwtService.gerarToken(usuario));
+        String token = jwtService.gerarToken(usuario);
+        System.out.println(token);
+
+        boolean isTokenValido = jwtService.tokenValido(token);
+        System.out.println("O token está válido? " + isTokenValido);
+
+        System.out.println(jwtService.obterUsuario(token));
     }
 
 }
